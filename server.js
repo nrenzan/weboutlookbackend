b1bcd31047492bb782c4b1d54866ddfec69d3ca1
nrenzan <69753426+nrenzan@users.noreply.github.com>
2022-04-15 12:26:23 +0545

    Backend complete

---
+++ server.js
@@ -0,0 +1,31 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
+
+//Handling Uncaught Exception
+process.on('uncaughtException', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Promise Rejection`);
+  process.exit(1);
+});
+// Error: console.log(youtube);
+
+//Config
+dotenv.config({ path: 'backend/config/config.env' });
+
+//connecting to database
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is running on http://localhost:${process.env.PORT}`);
+});
+
+//Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+  console.log(`Error : ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+//If error in connection unhandled rejection
